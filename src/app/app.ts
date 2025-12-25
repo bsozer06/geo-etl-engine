9fd70dcdf505febf8211f8f4e5b3fd534ad5bf16
bsozer06 <bsozer06@gmail.com>
2025-12-25 18:55:28 +0300

    feat: add upload component for KML file import and integrate into app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { MapViewerComponent } from './components/map-viewer/map-viewer.component';
+import { UploadComponent } from './components/upload/upload.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ MapViewerComponent],
+  imports: [ MapViewerComponent, UploadComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
